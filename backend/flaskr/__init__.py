b74fd057dac565cbad46d2b0d4e56a0bae6ff356
nsmichelson <nsmichelson@penguin>
2020-01-17 15:21:29 -0800

    Added some unit tests

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -146,18 +146,6 @@ def create_app(test_config=None):
     })
 
 
-  '''
-    @TODO: 
-    Create a POST endpoint to get questions to play the quiz. 
-    This endpoint should take category and previous question parameters 
-    and return a random questions within the given category, 
-    if provided, and that is not one of the previous questions. 
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not. 
-  '''
-
   @app.route('/api/quizzes',methods=['POST'])
   def quizMaker():
     quiz_category = request.get_json().get('quiz_category')
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+import random
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -15,7 +16,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.username = 'nsmichelson'
+        self.password = 'nsmichelson'
+        self.database_path = "postgres://{}:{}@{}/{}".format(self.username,self.password,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -29,10 +32,49 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    #get questions successfully
+    def test_getCategories(self):
+        response = self.client().get('/api/categories')
+
+        data = response.json()
+
+        self.assertEqual(data['success'],True)
+        self.assetTrue(data['categories'])
+
+    #get cateogires successfully
+    def test_getQuestions(self):
+        response =self.client().get('/api/questions')
+        data = json.loads(response.data)
+
+        self.assertEqual(date['success'],True)
+        self.assertTrue(date['questions'])
+    #delete a question successfully
+    def test_deletesQ(self):
+        random_q = random.choice([question.id for question in Question.query.all()])
+        response = self.client().delete('api/questions/{}'.format(random_q))
+
+        self.assertEqual(["success"],True)
+        self.assertTrue(data['questions'])
+    #create a quiz
+
+    def test_quiz(self):
+        random_category = random.choice([category.id for category in Category.query.all()])
+        response = self.client().post('/api/quizzes',json={'previous_questions': [],'quiz_category': {'type': 'Art','id': '2'}})
+
+        data = json.loads(response.data)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['question'])
+
+         
+    #add a question
+    def test_addQ(self):
+        qToAdd  = {'question':'Test','answer':'Test Answer','category':1,'difficulty':1}
+        response=self.client().post('/api/questions',json=qToAdd)
+
+        data = json.loads(response.data)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['created'])
+
 
 
 # Make the tests conveniently executable
