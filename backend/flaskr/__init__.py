4acb2d47f150ddc54f7b9a4045466d6ad281d424
nsmichelson <nsmichelson@penguin>
2020-12-11 11:52:16 -0800

    pretty close to getting code to comply with the style guide

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -42,36 +42,36 @@ def create_app(test_config=None):
                              'true')
         return response
 
-    #GET request to get a list of all the category names and their ids
-    @app.route('/api/categories',methods=["GET"])
+    # GET request to get a list of all the category names and their ids
+    @app.route('/api/categories', methods=["GET"])
     def getCategories():
         categories = Category.query.order_by(Category.id).all()
 
         CategoriesFormatted = []
-        
+
         for category in categories:
             cleanCategory = Category.format(category)
             CategoriesFormatted.append(cleanCategory)
-            print("categories formatted is",CategoriesFormatted)
+            print("categories formatted is", CategoriesFormatted)
 
-        other = {category.id : category.type for category in categories}
+        other = {category.id: category.type for category in categories}
 
         if categories is None:
-          abort(404)
+            abort(404)
 
         return jsonify({
-            "Success":True,
-            "Total_Categories":len(Category.query.all()),
-            "categories":other
+            "Success": True,
+            "Total_Categories": len(Category.query.all()),
+            "categories": other
             })
 
-    #GET request to retrieve all the trivia questions (for main page)
+    # GET request to retrieve all the trivia questions (for main page)
     @app.route('/api/questions')
     def getQuestions():
         selection = Question.query.order_by(Question.id).all()
-        current_questions = paginate_questions(request,selection)
+        current_questions = paginate_questions(request, selection)
 
-        if len(current_questions)==0:
+        if len(current_questions) == 0:
             abort(404)
 
         categoryList = []
@@ -82,60 +82,64 @@ def create_app(test_config=None):
 
         categories = Category.query.all()
         totalQuestions = len(Question.query.all())
-          
-        categoriesReturn = {category.id : category.type for category in categories} 
+
+        categoriesReturn =
+        {category.id: category.type for category in categories}
 
         return jsonify({
-            "success":True,
+            "success": True,
             "questions": current_questions,
             "totalQuestions": totalQuestions,
-            "categories":categoriesReturn,
-            "current_category": list(set([question['category'] for question in current_questions]))
+            "categories": categoriesReturn,
+            "current_category":
+            list(set([question['category'] for question in current_questions]))
             })
-    
-    #route to delete a specific question  
+
+    # route to delete a specific question
     @app.route('/api/questions/<int:question_id>', methods=["DELETE"])
     def deleteQuestion(question_id):
         try:
-            question = Question.query.filter(Question.id == question_id).one_or_none()
-            
+            question =
+            Question.query.filter(Question.id == question_id).one_or_none()
+
             if question is None:
                 abort(404)
-              
+
             question.delete()
 
             selection = Question.query.order_by(Question.id).all()
 
-            current_questions = paginate_questions(request,selection)
+            current_questions = paginate_questions(request, selection)
 
             return jsonify({
-                "success":True,
-                "questions":current_questions
+                "success": True,
+                "questions": current_questions
                 })
         except:
             abort(422)
 
-
-    #route to look up the questions from one particular category
-    @app.route('/api/categories/<int:category_id>/questions',methods=['GET'])
+    # route to look up the questions from one particular category
+    @app.route('/api/categories/<int:category_id>/questions', methods=['GET'])
     def qforcategory(category_id):
-        selection = Question.query.filter(Question.category==category_id)
-        
-        current_questions = paginate_questions(request,selection)
+
+        selection = Question.query.filter(Question.category == category_id)
+
+        current_questions = paginate_questions(request, selection)
 
         return jsonify({
-            "success":True,
-            "questions":current_questions
+            "success": True,
+            "questions": current_questions
             })
 
-
-    #route to generate a quiz based on a category of choosing
-    @app.route('/api/quizzes',methods=['POST'])
+    # route to generate a quiz based on a category of choosing
+    @app.route('/api/quizzes', methods=['POST'])
     def quizMaker():
         quiz_category = request.get_json().get('quiz_category')
         previous_questions = request.get_json().get('previous_questions')
         if quiz_category['id']:
-            questions = Question.query.filter(Question.category == quiz_category['id']).order_by(Question.id).all() 
+            questions =
+            Question.query.filter(Question.category ==
+                quiz_category['id']).order_by(Question.id).all()
         else:
             questions = Question.query.all()
 
@@ -143,33 +147,33 @@ def create_app(test_config=None):
 
         for question in questions:
             if question.id not in previous_questions:
-                questionOptions.append(question.format()) 
+                questionOptions.append(question.format())
 
-        #qToAdd = Question.query.get(question.)
+        # qToAdd = Question.query.get(question.)
         if questionOptions:
             return jsonify({
-                "success":True,
+                "success": True,
                 "question": random.choice(questionOptions),
                 "forceEnd": False
                 })
-        #dealing with when run out of questions
+        # dealing with when run out of questions
         else:
             return jsonify({
-                "success":True,
+                "success": True,
                 "question": '',
-                "forceEnd":True
+                "forceEnd": True
                 })
 
-    
     @app.route('/api/questions', methods=["POST"])
     def addQuestion():
         searchTerm = request.get_json().get('searchTerm')
-    
+
         if searchTerm:
             searchterm = request.get_json().get('searchTerm')
-            selection = Question.query.order_by(Question.id).filter(Question.question.ilike('%{}%'.format(searchterm)))
+            selection =
+            Question.query.order_by(Question.id).filter(Question.question.ilike('%{}%'.format(searchterm)))
             current_questions = paginate_questions(request, selection)
-            
+
             return jsonify({
                 'success': True,
                 'questions': current_questions
@@ -177,10 +181,10 @@ def create_app(test_config=None):
 
         else:
             body = request.get_json()
-            new_question = body.get('question',None)
-            new_answer = body.get('answer',None)
-            new_difficulty = int(body.get('difficulty',None))
-            new_category = int(body.get('category',None))
+            new_question = body.get('question', None)
+            new_answer = body.get('answer', None)
+            new_difficulty = int(body.get('difficulty', None))
+            new_category = int(body.get('category', None))
 
             try:
                 newQuestion = Question(question=new_question, answer=new_answer, category=new_category, difficulty=new_difficulty)
@@ -188,20 +192,19 @@ def create_app(test_config=None):
                 newQuestion.insert()
 
                 return jsonify({
-                    "success":True,
-                    "created":newQuestion.id
+                    "success": True,
+                    "created": newQuestion.id
                     })
             except:
                 print("something went wrong")
                 print("Unexpected error:", sys.exc_info()[0])
                 raise
-                #abort(422)
-        
-    
+                # abort(422)
+
     @app.errorhandler(404)
     def not_found(error):
         return jsonify({
-            "success": False, 
+            "success": False,
             "error": 404,
             "message": "resource not found"
             }), 404
@@ -209,7 +212,7 @@ def create_app(test_config=None):
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
-            "success": False, 
+            "success": False,
             "error": 422,
             "message": "unprocessable"
             }), 422
@@ -217,14 +220,9 @@ def create_app(test_config=None):
     @app.errorhandler(400)
     def bad_request(error):
         return jsonify({
-            "success": False, 
+            "success": False,
             "error": 400,
             "message": "bad request"
             }), 400
 
-    
-    
-    
     return app
-
-    
